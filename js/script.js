56b71f21ea1e46d490e76b2e9af66c2ba8c51028
Chris Robbins <Jaguar85>
2016-10-07 12:43:41 -0500

    changed urls back

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ function deleteBand() {
 
 function loadBands() {
   $.ajax({
-      url: 'https://git.heroku.com/bandlistapi.git'
+      url: 'https://localhost:3000/bands'
     }).done(function(data) {
       console.log(data);
       for (var i = 0; i < data.length; i++) {
@@ -103,7 +103,7 @@ function loadBand(band) {
   var li = $('<li></li>');
   li.html(band.name + ' ');
    var a = $('<a>Delete</a>');      
-   a.attr('href','https://git.heroku.com/bandlistapi.git' + band._id);     a.addClass('deleteBand');    
+   a.attr('href','http://localhost:3000/bands/' + band._id);     a.addClass('deleteBand');    
    li.append(a);    
    $('#band_list').prepend(li);
 };
@@ -113,7 +113,7 @@ function loadNewBand() {
     var bandName = $('#band_name').val()
     var genre = $('#genre').val()
     $.ajax({
-      url: 'https://git.heroku.com/bandlistapi.git',
+      url: 'http://localhost:3000/bands',
       method: 'POST',
       data: $('#new_band_form').serialize()
     }).done(function(newBand) {
